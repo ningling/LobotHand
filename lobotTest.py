8e98bf86830cced68adc505e28ad2618032761b1
Ning Ling <ningling@outlook.com>
2018-01-09 21:36:13 +0800

    Adding setSingleServo

--- lobotTest.py
+++ lobotTest.py
@@ -2,6 +2,5 @@ from lobot import LSC_board as lsc
 import numpy as np
 lscboard=lsc()
 #servo=np.matrix([[1,1500],[2,1500],[3,1500],[4,1500],[5,1500],[6,1500]])
-servo=np.matrix([[4,500]])
-lscboard.MultiServoMove(speed=10000,servoData=servo)
- 
+servo=np.matrix([[2,2000]])
+lscboard.MultiServoMove(speed=2000,servoData=servo)
